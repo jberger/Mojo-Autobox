7ac0e39d0c3e4f4ecdc02a5f057be2c62528f51f
Joel Berger <joel.a.berger@gmail.com>
2014-12-23 22:46:54 -0600

    added documentation for ojoBox

--- lib/ojoBox.pm
+++ lib/ojoBox.pm
@@ -10,3 +10,15 @@ sub import {
 
 1;
 
+=head1 NAME
+
+ojoBox - One-liners with the powers of ojo and Mojo::Autobox
+
+=head1 SYNOPSIS
+
+ $ perl -MojoBox -E 'g("http://mojolicio.us")->dom->find("a")->each(sub{$_->{href}->url->host->b->say})'
+
+=head1 DESCRIPTION
+
+Enables L<Mojo::Autobox> and L<ojo> for use in one-liners.
+
